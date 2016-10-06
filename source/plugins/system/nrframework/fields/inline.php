d943c02be1db4f051a36112f8c57db523ba07100
Tassos Marinos <info@tassos.gr>
2016-10-06 18:41:23 +0300

    Updates inline field

--- source/plugins/system/nrframework/fields/inline.php
+++ source/plugins/system/nrframework/fields/inline.php
@@ -15,46 +15,20 @@ require_once dirname(__DIR__) . '/helpers/field.php';
 class JFormFieldNR_Inline extends NRFormField
 {
 	/**
-	 * The field type.
-	 *
-	 * @var         string
-	 */
-	public $type = 'nr_inline';
-
-	protected function getLabel()
-	{
-		return '';
-	}
-
-	/**
 	 *  Method to render the input field
 	 *
 	 *  @return  string
 	 */
 	protected function getInput()
 	{
+		$start = $this->get('start', 1);
+		$end   = $this->get('end', 0);
 
-		JFactory::getDocument()->addStylesheet(JURI::root(true) . "/plugins/system/nrframework/assets/css/fields.css");
-
-		$title     = $this->get('label');
-		$class     = $this->get('class');
-		$start     = $this->get('start', 0);
-		$end       = $this->get('end', 0);
-
-		$html = array();
-
-		if ($start || !$end)
+		if ($start && !$end)
 		{
-			$html[] = '</div>';
-			$html[] = '<div class="inline-control-group' . $class . '">';
-			$html[] = '<div><div>';
+			return '<div class="inline-control-group '.$this->get('class').'"><div><div>';
 		}
 
-		if (!$start && !$end)
-		{
-			$html[] = '</div>';
-		}
-
-		return '</div>' . implode('', $html);
+		return '</div></div></div>';
 	}
 }

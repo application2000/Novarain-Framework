3f1d2e23a2167d81f3286fb0261544c8f5e28457
Sotiris Katsaniotis <sotiris.katsaniotis@gmail.com>
2016-10-11 14:48:13 +0300

    Implements the NR_ActiveCampaign wrapper

---
+++ source/plugins/system/nrframework/helpers/wrappers/activecampaign.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @package         @pkg.name@
+ * @version         @pkg.version@ @vUf@
+ *
+ * @author          Tassos Marinos <info@tassos.gr>
+ * @link            http://www.tassos.gr
+ * @copyright       Copyright © 2016 Tassos Marinos All Rights Reserved
+ * @license         GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+require_once __DIR__ . '/wrapper.php';
+
+class NR_ActiveCampaign extends NR_Wrapper
+{
+	/**
+	 * Create a new instance
+	 * @param string $api_key Your ActiveCampaign API key
+	 * @throws \Exception
+	 */
+	public function __construct($api_key, $url)
+	{
+		parent::__construct();
+		$this->setApiKey($api_key);
+		$this->setApiEndpoint($url);
+		$this->options->set('headers.Accept', 'application/json; charset=utf-8');
+		$this->options->set('headers.Content-Type', 'application/x-www-form-urlencoded');
+		$this->options->set('follow_location', true);
+	}
+
+	/**
+	 * Setter method for the API Endpoint
+	 * @param string $url The URL which is set in the account's developer settings
+	 * @throws \Exception 
+	 */
+	public function setApiEndpoint($url)
+	{
+		if (!empty($url))
+		{
+			$query              = http_build_query(array('api_key' => $this->api_key, 'api_output' => 'json'));
+			$this->api_endpoint = $url . '/admin/api.php?' . $query;
+		}
+		else
+		{
+			throw new \Exception("Invalid ActiveCampaign URL `{$url}` supplied.");
+		}
+	}
+}

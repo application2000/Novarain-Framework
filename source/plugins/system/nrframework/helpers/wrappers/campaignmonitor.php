86c3eb45e0122933757c9b1753c00f1c8d4df511
Sotiris Katsaniotis <sotiris.katsaniotis@gmail.com>
2016-10-18 21:42:52 +0300

    Modifies NR_CampaignMonitor to work with OAuth 2.0

--- source/plugins/system/nrframework/helpers/wrappers/campaignmonitor.php
+++ source/plugins/system/nrframework/helpers/wrappers/campaignmonitor.php
@@ -19,17 +19,14 @@ class NR_CampaignMonitor extends NR_Wrapper
 {
 	/**
 	 * Create a new instance
-	 * @param string $api_key Your CampaignMonitor API key
+	 * @param string $key Your CampaignMonitor Access Token
 	 * @throws \Exception
 	 */
-	public function __construct($api_key)
+	public function __construct($key)
 	{
 		parent::__construct();
-		$this->setApiKey($api_key);
-		$this->api_endpoint = 'https://api.createsend.com/api/v3.1';
-		$this->options->set('headers.Accept', 'application/json; charset=utf-8');
-		$this->options->set('headers.Content-Type', 'application/json; charset=utf-8');
-		$this->options->set('userauth',$this->api_key);
-		$this->options->set('passwordauth','nopass');
+		$this->setKey($key);
+		$this->setEndpoint('https://api.createsend.com/api/v3.1');
+		$this->options->set('headers.Authorization', 'Bearer ' . $this->key);
 	}
 }

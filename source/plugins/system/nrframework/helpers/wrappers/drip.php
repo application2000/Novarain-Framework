0f073fddd56862a5684c4e2b4bf260d2dc70d289
Sotiris Katsaniotis <sotiris.katsaniotis@gmail.com>
2016-10-18 21:43:10 +0300

    Modifies NR_Drip to work with OAuth 2.0

--- source/plugins/system/nrframework/helpers/wrappers/drip.php
+++ source/plugins/system/nrframework/helpers/wrappers/drip.php
@@ -19,17 +19,16 @@ class NR_Drip extends NR_Wrapper
 {
 	/**
 	 * Create a new instance
-	 * @param string $api_key Your Drip API key
+	 * @param string $key Your Drip API key
 	 * @throws \Exception
 	 */
-	public function __construct($api_key)
+	public function __construct($key)
 	{
 		parent::__construct();
-		$this->setApiKey($api_key);
-		$this->api_endpoint = 'http://api.getdrip.com/v2';
-		$this->options->set('headers.Accept', 'application/json; charset=utf-8');
-		$this->options->set('headers.Content-Type', 'application/json; charset=utf-8');
-		$this->options->set('userauth',$this->api_key);
-		$this->options->set('passwordauth','');
+		$this->setKey($key);
+		$this->setEndpoint('http://api.getdrip.com/v2');
+		$this->options->set('headers.Accept', 'application/vnd.api+json');
+		$this->options->set('headers.Content-Type', 'application/vnd.api+json');
+		$this->options->set('headers.Authorization', 'Bearer ' . $this->key);
 	}
 }

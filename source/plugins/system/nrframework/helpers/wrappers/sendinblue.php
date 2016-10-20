e6b9a434eb60a4f292d2f2aed3c694a946fcad27
Sotiris Katsaniotis <sotiris.katsaniotis@gmail.com>
2016-10-20 23:17:16 +0300

    Implements the NR_SendInBlue Wrapper

---
+++ source/plugins/system/nrframework/helpers/wrappers/sendinblue.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package         @pkg.name@
+ * @version         @pkg.version@ @vUf@
+ *
+ * @author          Tassos Marinos <info@tassos.gr>
+ * @link            http://www.tassos.gr
+ * @copyright       Copyright © 2016 Tassos Marinos All Rights Reserved
+ * @license         GNU GPLv3 <http://www.gnu.org/licenses/gpl.html> or later
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+require_once __DIR__ . '/wrapper.php';
+
+class NR_SendInBlue extends NR_Wrapper
+{
+	/**
+	 * Create a new instance
+	 * @param string $key Your SendInBLue API Key
+	 * @throws \Exception
+	 */
+	public function __construct($key)
+	{
+		parent::__construct();
+		$this->setKey($key);
+		$this->setEndpoint('https://api.sendinblue.com/v2.0');
+		$this->options->set('headers.api-key', $this->key);
+	}
+}

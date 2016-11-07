e60eb86763a8e76d5b188fe636b6c1ebd8c255a3
Tassos Marinos <info@tassos.gr>
2016-11-07 12:36:49 +0200

    Adds missing comments to Cache class

--- source/plugins/system/nrframework/helpers/cache.php
+++ source/plugins/system/nrframework/helpers/cache.php
@@ -9,15 +9,38 @@
 
 defined('_JEXEC') or die;
 
+/**
+ *  Caching mechanism
+ */
 class NRCache
 {
+
+	/**
+	 *  Static cache object
+	 *
+	 *  @var  array
+	 */
 	static $cache = array();
 
+	/**
+	 *  Check if has alrady exists in memory
+	 *
+	 *  @param   string   $hash  The hash string
+	 *
+	 *  @return  boolean         
+	 */
 	static public function has($hash)
 	{
 		return isset(self::$cache[$hash]);
 	}
 
+	/**
+	 *  Returns hash valuw
+	 *
+	 *  @param   string  $hash  The hash string
+	 *
+	 *  @return  mixed          False on error, Object on success
+	 */
 	static public function get($hash)
 	{
 		if (!self::has($hash))
@@ -28,12 +51,28 @@ class NRCache
 		return is_object(self::$cache[$hash]) ? clone self::$cache[$hash] : self::$cache[$hash];
 	}
 
+	/**
+	 *  Sets on memory the hash value
+	 *
+	 *  @param  string  $hash  The hash string
+	 *  @param  mixed   $data  Can be string or object
+	 *
+	 *  @return mixed
+	 */
 	static public function set($hash, $data)
 	{
 		self::$cache[$hash] = $data;
 		return $data;
 	}
 
+	/**
+	 *  Reads hash valuw from memory or file
+	 *
+	 *  @param   string   $hash   The hash string
+	 *  @param   boolean  $force  If true, the filesystem will be used as well on the /cache/ folder
+	 *
+	 *  @return  mixed            The hash object valuw
+	 */
 	static public function read($hash, $force = false)
 	{
 		if (self::has($hash))
@@ -51,6 +90,15 @@ class NRCache
 		return $cache->get($hash);
 	}
 
+	/**
+	 *  Writes hash value in cache folder
+	 *
+	 *  @param   string   $hash  The hash string
+	 *  @param   mixed    $data  Can be string or object
+	 *  @param   integer  $ttl   Expiration duration in milliseconds
+	 *
+	 *  @return  mixed           The hash object value
+	 */
 	static public function write($hash, $data, $ttl = 0)
 	{
 		$cache = JFactory::getCache('novarain','');

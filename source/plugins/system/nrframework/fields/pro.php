5cb4b156ef24685ed61fd4f0dd3a008397783761
Tassos Marinos <info@tassos.gr>
2017-01-23 12:05:44 +0200

    Adds UTM parameters support to the PRO field

--- source/plugins/system/nrframework/fields/pro.php
+++ source/plugins/system/nrframework/fields/pro.php
@@ -15,19 +15,34 @@ require_once dirname(__DIR__) . '/helpers/field.php';
 class JFormFieldNR_PRO extends NRFormField
 {
     /**
-     * The field type.
-     *
-     * @var         string
-     */
-    public $type = 'pro';
-
-    /**
      *  Method to render the input field
      *
      *  @return  string  
      */
     protected function getInput()
     {   
-        return '<a style="float:none;" class="btn btn-danger ' . $this->get("class") . '" href="' . $this->get("url") . '" target="_blank"><span class="icon-lock"></span> '. $this->prepareText($this->get("link", "NR_UPGRADE_TO_PRO_TO_UNLOCK")) .'</a>';
+        $url = $this->getURL();
+
+        return '<a style="float:none;" class="btn btn-danger ' . $this->get("class") . '" href="' . $url . '" target="_blank"><span class="icon-lock"></span> '. $this->prepareText($this->get("link", "NR_UPGRADE_TO_PRO_TO_UNLOCK")) .'</a>';
+    }
+
+    /**
+     *  Constructs URL with Google Analytics Campaign Parameters
+     *
+     *  @return  string
+     */
+    private function getURL()
+    {
+        $url = $this->get("url");
+
+        if (!$this->get("addutm", true))
+        {
+            return $url;
+        }
+
+        $utm  = 'utm_source=Joomla&utm_medium=upgradebutton&utm_campaign=freeversion';
+        $char = strpos($url, "?") === false ? "?" : "&";
+
+        return $url . $char . $utm;
     }
 }

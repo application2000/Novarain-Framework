81900dc71cfc365ba2898650a60622f4074044cf
Tassos Marinos <info@tassos.gr>
2017-05-04 07:15:23 +0000

    Adds ShowSelect option to Modules field

--- source/plugins/system/nrframework/fields/modules.php
+++ source/plugins/system/nrframework/fields/modules.php
@@ -13,44 +13,45 @@ defined('_JEXEC') or die;
 jimport('joomla.form.helper');
 JFormHelper::loadFieldClass('list');
 
-class JFormFieldModules extends JFormFieldList
-{
-    /**
-     * The field type.
-     *
-     * @var         string
-     */
-    protected $type = 'modules';
+require_once JPATH_PLUGINS . '/system/nrframework/helpers/fieldlist.php';
 
+class JFormFieldModules extends NRFormFieldList
+{
     /**
      * Method to get a list of options for a list input.
      *
-     * @return      array           An array of JHtml options.
+     * @return   array   An array of JHtml options.
      */
-    protected function getOptions() 
+    protected function getOptions()
     {
         $db = JFactory::getDBO();
+
         $query = $db->getQuery(true);
-        $query->select('*');
-        $query->from('#__modules');
-        $query->where('published=1'); 
-        $query->where('access !=3'); 
-        $query->order('title'); 
+
+        $query->select('*')
+            ->from('#__modules')
+            ->where('published=1')
+            ->where('access !=3')
+            ->order('title');
+
         $rows = $db->setQuery($query);              
         $results = $db->loadObjectList();
 
-        if ($db->getErrorNum()) {
-            echo $db->stderr();
-            return false;
-        }
-
         $options = array();
 
-        foreach ($results as $option) {
+        $showSelect = $this->get("showselect", "true") == "true" ? true : false;
+        if ($showSelect)
+        {
+            $options[] = JHTML::_('select.option', "", '- ' . JText::_("NR_SELECT_MODULE") . ' -');
+        }
+
+        foreach ($results as $option)
+        {
             $options[] = JHTML::_('select.option', $option->id, $option->title);
         }
 
         $options = array_merge(parent::getOptions(), $options);
+
         return $options;
     }
 }

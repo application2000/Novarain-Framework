25005000c92b01e1742d482466a7f9718442be4c
Tassos Marinos <info@tassos.gr>
2018-12-11 12:14:41 +0200

    UpdateSites: Remove duplicate entries, stop clearing cache, use Https instead of Http.

--- source/plugins/system/nrframework/NRFramework/Updatesites.php
+++ source/plugins/system/nrframework/NRFramework/Updatesites.php
@@ -25,7 +25,7 @@ class Updatesites
 	 *
 	 *  @param  string  $key  Download Key
 	 */
-	function __construct($key = null)
+	public function __construct($key = null)
 	{
 		$this->db  = \JFactory::getDBO();
 		$this->key = ($key) ? $key : $this->getDownloadKey();
@@ -36,10 +36,11 @@ class Updatesites
    	 */
    	public function update()
    	{
-   		$this->updateDownloadKey();
-		$this->purgeUpdateSites();
-   	}
-
+		$this->removeDuplicates();
+		$this->updateHttptoHttps();
+		$this->updateDownloadKey();
+	}
+	   
 	/**
 	 *  Reads the Download Key saved in the Novarain Framework system plugin parameters
 	 *
@@ -87,17 +88,80 @@ class Updatesites
 	}
 
 	/**
-	 *  Removes all of the updates from the table
+	 * Update http to https
 	 *
-	 *  @return  void
+	 * @return void
 	 */
-	private function purgeUpdateSites()
+	private function updateHttptoHttps()
 	{
-        \JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_installer/models');
+		$query = $this->db->getQuery(true)
+			->update('#__update_sites')
+			->set($this->db->quoteName('location') . ' = REPLACE('
+				. $this->db->quoteName('location') . ', '
+				. $this->db->quote('http://') . ', '
+				. $this->db->quote('https://')
+				. ')')
+			->where($this->db->quoteName('location') . ' LIKE ' . $this->db->quote('%tassos.gr%'));
+		$this->db->setQuery($query);
+		$this->db->execute();
+	}
 
-        if ($model = \JModelLegacy::getInstance('Update', 'InstallerModel', array('ignore_request' => true)))
-        {
-	        $model->purge();
-        }
+	/**
+	 * Remove duplicate update sites created by upgrading from Free to Pro version
+	 *
+	 * @return void
+	 */
+	private function removeDuplicates()
+	{
+		$db = $this->db;
+
+		// Find duplicates first
+		$query = 'SELECT name, COUNT(*) c FROM #__update_sites where location like "%tassos.gr%" GROUP BY name HAVING c > 1';
+		$db->setQuery($query);
+
+		if (!$duplicates = $db->loadObjectList())
+		{
+			return;
+		}
+
+		// OK we have duplicates. Let's remove them.
+		foreach ($duplicates as $key => $duplicate)
+		{
+			// Get all IDs
+			$query = $db->getQuery(true)
+				->select('update_site_id')
+				->from('#__update_sites')
+				->where('name = ' . $db->quote($duplicate->name))
+				->order('update_site_id DESC');
+
+			$db->setQuery($query);
+
+			if (!$update_site_ids = $db->loadObjectList())
+			{
+				return;
+			}
+
+			// Skip the 1st index which represents the last created and valid.
+			unset($update_site_ids[0]);
+
+			foreach ($update_site_ids as $key => $update_site_id)
+			{
+				$id = $update_site_id->update_site_id;
+
+				$query->clear()
+					->delete('#__update_sites')
+					->where($db->quoteName('update_site_id') . ' = ' . (int) $id);
+
+				$db->setQuery($query);
+				$db->execute();
+	
+				$query->clear()
+					->delete('#__update_sites_extensions')
+					->where($db->quoteName('update_site_id') . ' = ' . (int) $id);
+
+				$db->setQuery($query);
+				$db->execute();
+			}
+		}
 	}
 }
